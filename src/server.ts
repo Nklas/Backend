8cf26728163b862593ace8b136edee63304b8f4d
Nklas <niklaus462@gmail.com>
2026-01-26 18:27:09 +0200

    Added Swagger.

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import app from "./app";
 
-const app: Application = express();
-const port = 3000; // The port your express server will be running on.
+const port = 3000;
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
-});
-
-// POST route example
-app.post('/api/data', (req: Request, res: Response) => {
-  const data = req.body;
-  console.log('Received data:', data);
-  res.status(201).json({ message: 'Data received successfully', data });
-});
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Swagger available at http://localhost:${port}/api-docs`);
 });
---
+++ src/swagger.ts
@@ -0,0 +1,20 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Express API",
+      version: "1.0.0",
+      description: "API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/**/*.ts"],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
